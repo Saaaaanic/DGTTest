1e27473e65bb606aac5de49cb3da78f32cef0217
Saaaaanic <you@example.com>
2023-03-19 20:05:23 +0400

    Add Search

--- DGTTest/ViewModels/MainPageVM.cs
+++ DGTTest/ViewModels/MainPageVM.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Navigation;
 using DGTTest.Models;
 using DGTTest.Models.Service;
@@ -14,20 +15,34 @@ namespace DGTTest;
 public class MainPageVM : BindableBase, IDataErrorInfo
 {
     private readonly CurrencyAPI _modelCurrencies = new CurrencyAPI();
+    private CollectionViewSource _currencies = new CollectionViewSource();
     
     public MainPageVM()
     {
-        _modelCurrencies.PropertyChanged += (obj, e) => { RaisePropertyChanged(e.PropertyName); };
+        _currencies.Source = _modelCurrencies.PublicCurrencies;
         RefreshList = new DelegateCommand<string>((str) =>
         {
             var num = Int32.Parse(str);
             if(num > 0 && num <= 100) _modelCurrencies.GetTopCurrencies(CountOfCurrencies).Wait();
         });
+        Search = new DelegateCommand<string>((str) =>
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                Currencies.View.Filter = null;
+            }
+            else
+            {
+                Currencies.View.Filter = item => item is Currency currency && currency.Name.StartsWith(str, StringComparison.InvariantCultureIgnoreCase);
+                Currencies.View.Refresh();
+            }
+        });
     }
 
     public int CountOfCurrencies { get; set; }
-    public ReadOnlyObservableCollection<Currency> Currencies => _modelCurrencies.PublicCurrencies;
+    public CollectionViewSource Currencies => _currencies;
     public DelegateCommand<string> RefreshList { get; }
+    public DelegateCommand<string> Search { get; }
 
     // Implementation of IDataErrorInfo interface
     public string Error { get; }
